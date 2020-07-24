2beac7540e749a3e91b6321fb775ff86a5a4a7bc
nicomedin <nicolas.medina.sanchez@teracode.comm>
2020-07-24 00:32:10 -0300

    swap endpoint added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const userRoute = require('./user')
+const swapsRoute = require('./swap')
 
 const apiRouter = express.Router()
 
 apiRouter.use('/user', userRoute)
+apiRouter.use('/swap', swapsRoute)
 
 module.exports = apiRouter
